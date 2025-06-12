5ced1cb4940833cf6976bb1cb110b641fae6b5ba
jonathan vanasco <jonathan@2xlp.com>
2025-06-12 11:47:52 -0400

    add some debugging info

--- src/peter_sslers/lib/db/actions.py
+++ src/peter_sslers/lib/db/actions.py
@@ -1785,7 +1785,7 @@ def routine__renew_expiring(
         print("---")
         print("Expiring Certs @ ", ctx.timestamp)
         for cert in expiring_certs:
-            print(cert.id, cert.timestamp_not_after)
+            print(cert.id, cert.timestamp_not_after, cert.cert_issuer, cert.domains_as_string, )
         print("---")
 
     if DEBUG_LOCAL or dry_run:
